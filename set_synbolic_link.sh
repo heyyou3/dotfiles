a7850c6e49cd29bf1f95638307ce7f3bd4c0c82b
n_yuhei <nishidayuuhei@gmail.com>
2018-12-25 09:53:58 +0900

    Fix "~" to "$HOME"

--- set_synbolic_link.sh
+++ set_synbolic_link.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set_synbolic_link() {
-  if [ -e "$1" && -e "$2" ]; then
-    mv "$1" "$1".org
-    ln -s "$2" "$1"
+  if ! [ -e "$1" ]; then
+    ln -sf "$2" "$1"
   else
-    echo 'Argument file not found...'
+    echo "$1 found."
+    echo "Create $1.org"
+    mv "$1" "$1".org
+    ln -sf "$2" "$1"
   fi
 }
-set_synbolic_link '~/.vimrc' '~/dotfiles/vim/_vimrc'
-set_synbolic_link '~/.zshrc' '~/dotfiles/zsh/_zshrc'
-set_synbolic_link '~/.vim/rc/dein.toml' '~/dotfiles/vim/_dein.toml'
-set_synbolic_link '~/.vim/rc/dein_lazy.toml' '~/dotfiles/vim/_dein_lazy.toml'
-set_synbolic_link '~/.config/nvim/init.vim' '~/dotfiles/vim/_init.vim'
+set_synbolic_link "$HOME/.vimrc" "$HOME/dotfiles/vim/_vimrc"
+set_synbolic_link "$HOME/.zshrc" "$HOME/dotfiles/zsh/_zshrc"
+set_synbolic_link "$HOME/.vim/rc/dein.toml" "$HOME/dotfiles/vim/_dein.toml"
+set_synbolic_link "$HOME/.vim/rc/dein_lazy.toml" "$HOME/dotfiles/vim/_dein_lazy.toml"
+set_synbolic_link "$HOME/.config/nvim/init.vim" "$HOME/dotfiles/vim/_init.vim"
