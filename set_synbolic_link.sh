681108ba1a947b3a6d88c661b1d639405ae58276
n_yuhei <nishidayuuhei@gmail.com>
2019-02-03 18:16:56 +0900

    Delete files

--- set_synbolic_link.sh
+++ set_synbolic_link.sh
@@ -11,8 +11,4 @@ set_synbolic_link() {
 }
 set_synbolic_link "$HOME/.vimrc" "$HOME/dotfiles/vim/_vimrc"
 set_synbolic_link "$HOME/.zshrc" "$HOME/dotfiles/zsh/_zshrc"
-set_synbolic_link "$HOME/.vim/rc/dein.toml" "$HOME/dotfiles/vim/_dein.toml"
-set_synbolic_link "$HOME/.vim/rc/dein_lazy.toml" "$HOME/dotfiles/vim/_dein_lazy.toml"
-set_synbolic_link "$HOME/.config/nvim/init.vim" "$HOME/dotfiles/vim/_init.vim"
 set_synbolic_link "$HOME/.tmux.conf" "$HOME/dotfiles/tmux/_tmux.conf"
-set_synbolic_link "$HOME/.config/alacritty/alacritty.yml" "$HOME/dotfiles/alacritty/alacritty.yml"
