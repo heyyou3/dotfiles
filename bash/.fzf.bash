b34262165d49c46bcebd3c75dd59045021f74fcc
heyyou <hey3you3@gmail.com>
2021-01-11 12:58:26 +0900

    fix(dev_env_container): fzf.bash に HOME を指定するように変更

--- bash/.fzf.bash
+++ bash/.fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/heyyou/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/heyyou/.fzf/bin"
+if [[ ! "$PATH" == "*$HOME/.fzf/bin*" ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/heyyou/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/heyyou/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
