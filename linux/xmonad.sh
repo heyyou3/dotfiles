615d0dec98adb192b05906c738c09a9d01dc34ae
heyyou <hey3you3@gmail.com>
2021-01-31 20:43:58 +0900

    update(xmonad): xmonad 更新時に実行されるスクリプトを変更

--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 P=$(cat ~/start_config | base64 -d)
 
-echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
+if [ "$(ps aux | grep 'xkeysnail' | perl -nlE 'if($_!~/grep/){say $_}' | perl -nlE 'END{say $.}')" -le 0 ]; then
+  echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
+  sleep 2
+fi
+
+ps aux | grep 'xmobarrc' | perl -nlE 'if($_!~/grep/){say $_}' | perl -anlE 'system("kill $F[1]")'
+sleep 1
 /usr/bin/xmobar $HOME/.xmobarrc &
-sleep 2
 /usr/bin/xset r rate 300 99 &
-sleep 2
 synclient TouchpadOff=1
