eb3b9c6e134db0d91385a244e1326d7b6582da89
heyyou <hey3you3@gmail.com>
2021-02-04 03:33:00 +0900

    feat(linux): compton の設定ファイルと起動シェルを追加

--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
+
+# Run xmobar
 ps aux | grep 'xmobarrc' | perl -nlE 'if($_!~/grep/){say $_}' | perl -anlE 'system("kill $F[1]")'
-sleep 0.5
+sleep 0.2
 /usr/bin/xmobar $HOME/.xmobarrc &
-sleep 0.5
+sleep 0.2
+
+# View wallpaper
 /usr/bin/feh --bg-fill $HOME/dotfiles/wallpaper.png
 
+# Run xkeysnail
 P=$(cat ~/start_config | base64 -d)
-
 if [ "$(ps aux | grep 'xkeysnail' | perl -nlE 'if($_!~/grep/){say $_}' | perl -nlE 'END{say $.}')" -le 0 ]; then
   echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
-  sleep 0.5
+  sleep 0.2
 fi
 
-sleep 0.5
+# set xset rate
 /usr/bin/xset r rate 300 99 &
+
+sleep 0.2
+
+# Run compton
+compton --config $HOME/dotfiles/linux/compton.conf &
