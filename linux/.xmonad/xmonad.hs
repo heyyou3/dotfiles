bc27d4a120801cc9a3311485f993240db470a8d6
heyyou <hey3you3@gmail.com>
2021-02-03 22:17:25 +0900

    update(xmonad): xmonad のテーマを Solarize Dark に変更

--- linux/.xmonad/xmonad.hs
+++ linux/.xmonad/xmonad.hs
@@ -1,13 +1,79 @@
+import System.IO
+import System.Exit
 import XMonad
-import XMonad.Config.Xfce
-import XMonad.Util.Run (unsafeSpawn)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spiral
+import XMonad.Layout.PerWorkspace(onWorkspace)
+import XMonad.Layout.Tabbed
+import XMonad.Util.Run
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
-main = xmonad xfceConfig
-  {
+defaults = defaultConfig {
     terminal    = "/usr/local/bin/alacritty",
     modMask     = mod4Mask,
+    focusFollowsMouse = myFocusFollowsMouse,
+    borderWidth = myBorderWidth,
+    normalBorderColor = myNormalBorderColor,
+    focusedBorderColor = myFocusedBorderColor,
     startupHook = myStartupHook
-  }
+}
+
+main = do
+  xmonad $ defaults
 
 myStartupHook = do
   unsafeSpawn "$HOME/dotfiles/linux/xmonad.sh"
+
+defaultLayouts = avoidStruts (
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2)) |||
+    tabbed shrinkText tabConfig |||
+    Full |||
+    spiral (6/7)) |||
+    noBorders (fullscreenFull Full)
+
+------------------------------------------------------------------------
+-- Colors and borders
+--
+myNormalBorderColor = "#002b36"
+myFocusedBorderColor = "#657b83"
+
+-- Colors for text and backgrounds of each tab when in "Tabbed" layout.
+tabConfig = defaultTheme {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
+
+-- Color of current window title in xmobar.
+xmobarTitleColor = "green"
+
+-- Color of current workspace in xmobar.
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+-- Width of the window border in pixels.
+myBorderWidth = 1
+
+------------------------------------------------------------------------
+-- Mouse bindings
+--
+-- Focus rules
+-- True if your focus should follow your mouse cursor.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
+
+------------------------------------------------------------------------
+-- Floats all windows in a certain workspace.
+-- myLayouts
+myLayouts = onWorkspace "three" simplestFloat $ defaultLayouts
--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+ps aux | grep 'xmobarrc' | perl -nlE 'if($_!~/grep/){say $_}' | perl -anlE 'system("kill $F[1]")'
+sleep 0.5
+/usr/bin/xmobar $HOME/.xmobarrc &
+sleep 0.5
+/usr/bin/feh --bg-fill $HOME/dotfiles/wallpaper.png
+
 P=$(cat ~/start_config | base64 -d)
 
 if [ "$(ps aux | grep 'xkeysnail' | perl -nlE 'if($_!~/grep/){say $_}' | perl -nlE 'END{say $.}')" -le 0 ]; then
   echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
-  sleep 2
+  sleep 0.5
 fi
 
-ps aux | grep 'xmobarrc' | perl -nlE 'if($_!~/grep/){say $_}' | perl -anlE 'system("kill $F[1]")'
-sleep 1
-/usr/bin/xmobar $HOME/.xmobarrc &
+sleep 0.5
 /usr/bin/xset r rate 300 99 &
