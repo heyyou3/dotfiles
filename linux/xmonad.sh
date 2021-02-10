721fd96d27c8f077ab32b64993c12001784040a2
heyyou <hey3you3@gmail.com>
2021-02-11 04:02:07 +0900

    feat(xmonad): HHKB 接続時は HHKB を対象にして xkeysnail を実行するように実装

--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -5,10 +5,15 @@
 
 # Run xkeysnail
 P=$(cat ~/start_config | base64 -d)
-if [ "$(ps aux | grep 'xkeysnail' | perl -nlE 'if($_!~/grep/){say $_}' | perl -nlE 'END{say $.}')" -le 0 ]; then
+echo $P | sudo -S pkill 'xkeysnail'
+sleep 0.2
+if [ $(echo -e 'devices' | bluetoothctl | perl -anlE 'if($_=~/Device/&&$_=~/HHKB-BT/){say "info ".$F[1]}' | bluetoothctl | perl -anlE 'if($_=~/Connected:/){say $F[1]}') = 'yes' ]; then
+  hhkb_event=$(cat /proc/bus/input/devices | perl -anlE '$hhkb_i; if($_=~/HHKB-BT Keyboard/){$hhkb_i=$.}if($hhkb_i>4&&($hhkb_i+4)==$.){say $F[3]}')
+  echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py --device "/dev/input/$hhkb_event" &
+else
   echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
-  sleep 0.2
 fi
+sleep 0.2
 
 # set xset rate
 /usr/bin/xset r rate 300 99 &
