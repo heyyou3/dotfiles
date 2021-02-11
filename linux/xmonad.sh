f684994debd61e78c2129e62075d0143688892c2
heyyou <hey3you3@gmail.com>
2021-02-11 15:23:47 +0900

    thinkpad-LinuxMint での作業分を退避

--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -6,7 +6,6 @@
 # Run xkeysnail
 P=$(cat ~/start_config | base64 -d)
 echo $P | sudo -S pkill 'xkeysnail'
-sleep 0.2
 if [ $(echo -e 'devices' | bluetoothctl | perl -anlE 'if($_=~/Device/&&$_=~/HHKB-BT/){say "info ".$F[1]}' | bluetoothctl | perl -anlE 'if($_=~/Connected:/){say $F[1]}') = 'yes' ]; then
   hhkb_event=$(cat /proc/bus/input/devices | perl -anlE '$hhkb_i; if($_=~/HHKB-BT Keyboard/){$hhkb_i=$.}if($hhkb_i>4&&($hhkb_i+4)==$.){say $F[3]}')
   echo $P | sudo -S /usr/local/bin/xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py --device "/dev/input/$hhkb_event" &
