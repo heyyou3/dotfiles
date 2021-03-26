51a35aa76f5e015f1443ebda5389c067df83ccd0
heyyou <hey3you3@gmail.com>
2021-03-26 16:09:23 +0900

    update(xmonad): xkeysnail は初回起動しかしないように変更

--- linux/xmonad.sh
+++ linux/xmonad.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-
 # View wallpaper
 feh --bg-fill $HOME/dotfiles/wallpaper.png
 
 # Run xkeysnail
 P=$(cat ~/start_config | base64 -d)
-echo $P | sudo -S pkill 'xkeysnail'
-sleep 0.1
-if [ $(echo -e 'devices' | bluetoothctl | perl -anlE 'if($_=~/Device/&&$_=~/HHKB-BT/){say "info ".$F[1]}' | bluetoothctl | perl -anlE 'if($_=~/Connected:/){say $F[1]}') = 'yes' ]; then
-  hhkb_event=$(cat /proc/bus/input/devices | perl -anlE '$hhkb_i; if($_=~/HHKB-BT Keyboard/){$hhkb_i=$.}if($hhkb_i>4&&($hhkb_i+4)==$.){say $F[3]}')
-  echo $P | sudo -S xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py --device "/dev/input/$hhkb_event" &
-else
-  echo $P | sudo -S xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
+if [ $(ps aux | perl -nlE '$c;BEGIN{$c=0}if(/xkeysnail/&&!/grep xkeysnail/){$c++}END{say $c}') -eq 0 ]; then
+  if [ $(echo -e 'devices' | bluetoothctl | perl -anlE 'if($_=~/Device/&&$_=~/HHKB-BT/){say "info ".$F[1]}' | bluetoothctl | perl -anlE 'if($_=~/Connected:/){say $F[1]}') = 'yes' ]; then
+    hhkb_event=$(cat /proc/bus/input/devices | perl -anlE '$hhkb_i; if($_=~/HHKB-BT Keyboard/){$hhkb_i=$.}if($hhkb_i>4&&($hhkb_i+4)==$.){say $F[3]}')
+    echo $P | sudo -S xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py --device "/dev/input/$hhkb_event" &
+  else
+    echo $P | sudo -S xkeysnail $HOME/dotfiles/linux/xkeysnail/config.py &
+  fi
 fi
-sleep 0.1
 
 # set xset rate
 xset r rate 300 99 &
+
+xset s off -dpms
 
 sleep 0.1
 
