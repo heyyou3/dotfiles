cc6ae86ac91336682ec93c079a56e8bb3c977165
heyyou <hey3you3@gmail.com>
2021-08-16 07:46:49 +0900

    update(LANG): LANG 環境変数を en_US.UTF-8 に統一

--- nix_config/configuration.nix
+++ nix_config/configuration.nix
@@ -40,10 +40,15 @@ in {
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.inputMethod = {
-    enabled = "fcitx";
-    fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enabled = "fcitx";
+      fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
+    };
+    extraLocaleSettings = {
+      LC_MESSAGES = "en_US.UTF-8";
+    };
   };
   # console = {
   #   font = "Lat2-Terminus16";
