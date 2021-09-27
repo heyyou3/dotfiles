96b2bbb09e1db751ef7024e57a61b3285ffd5193
heyyou <hey3you3@gmail.com>
2021-09-27 23:57:18 +0900

    feat(nixos): tlp を導入

--- nix_config/configuration.nix
+++ nix_config/configuration.nix
@@ -50,19 +50,22 @@ in {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
   # };
-
   # Enable the X11 windowing system.
   services = {
     upower.enable = true;
     blueman.enable = true;
+
     xserver = {
       enable = true;
       layout = "us";
       desktopManager = {
         xterm.enable = false;
+        plasma5.enable = true;
       };
       displayManager = {
         defaultSession = "none+i3";
+        # Enable the Plasma 5 Desktop Environment.
+        sddm.enable = true;
       };
       windowManager.i3 = {
         enable = true;
@@ -75,46 +78,46 @@ in {
         ];
       };
     };
+
+    tlp.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      media-session.config.bluez-monitor.rules = [
+        {
+          # Matches all cards
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+          actions = {
+            "update-props" = {
+              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+              # mSBC is not expected to work on all headset + adapter combinations.
+              "bluez5.msbc-support" = true;
+              # SBC-XQ is not expected to work on all headset + adapter combinations.
+              "bluez5.sbc-xq-support" = true;
+            };
+          };
+        }
+        {
+          matches = [
+            # Matches all sources
+            { "node.name" = "~bluez_input.*"; }
+            # Matches all outputs
+            { "node.name" = "~bluez_output.*"; }
+          ];
+          actions = {
+            "node.pause-on-idle" = false;
+          };
+        }
+      ];
+    };
   };
 
-
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    media-session.config.bluez-monitor.rules = [
-      {
-        # Matches all cards
-        matches = [ { "device.name" = "~bluez_card.*"; } ];
-        actions = {
-          "update-props" = {
-            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-            # mSBC is not expected to work on all headset + adapter combinations.
-            "bluez5.msbc-support" = true;
-            # SBC-XQ is not expected to work on all headset + adapter combinations.
-            "bluez5.sbc-xq-support" = true;
-          };
-        };
-      }
-      {
-        matches = [
-          # Matches all sources
-          { "node.name" = "~bluez_input.*"; }
-          # Matches all outputs
-          { "node.name" = "~bluez_output.*"; }
-        ];
-        actions = {
-          "node.pause-on-idle" = false;
-        };
-      }
-    ];
-  };
+
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
