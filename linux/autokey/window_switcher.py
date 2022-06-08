7fcde2f0c38f21a8ec326da0d4b8e77c5a4dec57
heyyou <hey3you3@gmail.com>
2022-06-09 00:37:12 +0900

    update(autokey): autokey の window switcher の挙動を変更

--- linux/autokey/window_switcher.py
+++ linux/autokey/window_switcher.py
@@ -1,7 +1,9 @@
 # Enter script code
 from subprocess import run, PIPE
+import time
 run(['rofi', '-show', 'window', '-window-format', '[{n}]: {t}'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
 get_window_cmd = ['xdotool', 'getwindowfocus']
 window_num = run(get_window_cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 move_window_cmd = ['xdotool', 'mousemove', '--window', window_num.stdout, '--polar', '0', '0']
+time.sleep(0.2)
 run(move_window_cmd)
