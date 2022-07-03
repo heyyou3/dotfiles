54e274bbdd56bef9bd69db9a83860dff17d84389
heyyou <hey3you3@gmail.com>
2022-07-03 23:25:04 +0900

    update(autokey): change_window_size.py の window_size を % 指定に変更

--- linux/autokey/change_window_size.py
+++ linux/autokey/change_window_size.py
@@ -3,5 +3,5 @@
 from subprocess import PIPE, run
 get_window_cmd = ['xdotool', 'getwindowfocus']
 window_num = run(get_window_cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-move_window_cmd = ['xdotool', 'windowsize', window_num.stdout, '1700', '1000']
-run(move_window_cmd)
+change_window_size_cmd = ['xdotool', 'windowsize', window_num.stdout, '25%', '45%']
+run(change_window_size_cmd)
