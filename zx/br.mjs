505b50b8cb428a19ff17c1f78faca4343fb866ad
heyyou <hey3you3@gmail.com>
2025-07-06 15:22:32 +0900

    (zx)try,catch を追加して、コマンドをキャンセルした場合のエラーを表示しないように修正

--- zx/br.mjs
+++ zx/br.mjs
@@ -2,11 +2,15 @@
 import fs from "fs";
 import os from "os";
 
-const line = await $`cat ${os.homedir()}/dotfiles/.heyyou/bookmark.md | fzf`;
+try {
+  const line = await $`cat ${os.homedir()}/dotfiles/.heyyou/bookmark.md | fzf`;
 
-if (line) {
-  const url = String(line).match(/\((.*)\)/)?.[1];
-  if (url) {
-    await $`${process.env['BROWSER_BIN']} ${url}`;
+  if (line) {
+    const url = String(line).match(/\((.*)\)/)?.[1];
+    if (url) {
+      await $`${process.env['BROWSER_BIN']} ${url}`;
+    }
   }
+} catch (err) {
+  // pass
 }
