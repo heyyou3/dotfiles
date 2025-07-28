a5d43b5e57d7ff196b94f271d99dadd5e5fe69ec
heyyou <hey3you3@gmail.com>
2025-07-28 22:52:45 +0900

    (nvim)telescope.nvim を導入

--- nvim/init.lua
+++ nvim/init.lua
@@ -323,6 +323,10 @@ require("lazy").setup({
       },
     },
     {
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
+    },
+    {
       "mikavilpas/yazi.nvim",
       event = "VeryLazy",
       dependencies = {
@@ -340,8 +344,34 @@ require("lazy").setup({
       init = function()
         -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
       end,
-    }
+    },
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+require('telescope').setup{
+  defaults = {
+    -- レイアウトとサイズに関する設定
+    layout_strategy = 'horizontal',
+    layout_config = {
+      width = 0.95,
+      height = 0.90,
+    },
+
+    borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+    
+    sorting_strategy = 'ascending',
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+  },
+}
 
 --[[
 ================================================================================
