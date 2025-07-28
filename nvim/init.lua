786c01e06c551fe3b96eb889b4e3593c71bb175b
heyyou <hey3you3@gmail.com>
2025-07-28 23:49:39 +0900

    (nvim)yazi.nvimを削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -326,25 +326,6 @@ require("lazy").setup({
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
     },
-    {
-      "mikavilpas/yazi.nvim",
-      event = "VeryLazy",
-      dependencies = {
-        { "nvim-lua/plenary.nvim", lazy = true },
-      },
-      ---@type YaziConfig | {}
-      opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
-        open_for_directories = true,
-        keymaps = {
-          show_help = "<f1>",
-        },
-      },
-      -- 👇 if you use `open_for_directories=true`, this is recommended
-      init = function()
-        -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-      end,
-    },
 })
 
 local builtin = require('telescope.builtin')
