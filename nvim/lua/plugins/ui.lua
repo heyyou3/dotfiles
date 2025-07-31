399b828b8b21edd18f506d3681a3c8bd9e092052
heyyou <hey3you3@gmail.com>
2025-08-01 08:04:18 +0900

    (nvim)add dracula.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,15 +1,1 @@
-
-return {
-  "vim-airline/vim-airline",
-  event = "VimEnter",
-  dependencies = { "vim-airline/vim-airline-themes" },
-  config = function()
-    vim.g.airline_theme = "dracula"
-    vim.g.airline_solarized_bg = "dark"
-    vim.g.airline_section_a = vim.fn["airline#section#create"]({ "mode", "", "branch" })
-    vim.g["airline#extensions#tabline#enabled"] = 1
-    vim.g["airline#extensions#tabline#show_buffers"] = 0
-    vim.g["airline#extensions#tabline#tab_nr_type"] = 1
-    vim.g["airline#extensions#tabline#fnamemod"] = ":t"
-  end,
-}
+return {}
