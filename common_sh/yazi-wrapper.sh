4cc32069db7267a447d5af0059fe9e6893799e53
heyyou <hey3you3@gmail.com>
2025-08-02 01:01:51 +0900

    (common_sh)Neovimで現在開いているディレクトリを起点にして、yaziを実行するように変更

--- common_sh/yazi-wrapper.sh
+++ common_sh/yazi-wrapper.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # Read the address from the temp file
 window_name="$(cat /tmp/yazi_current_window_name)"
+
 export NVIM_LISTEN_ADDRESS="/tmp/$window_name"
 export EDITOR=nvr
 
-yazi
+cwd=$(nvr --remote-expr 'expand("%:p:h")' 2>/dev/null)
+if [ -z "$cwd" ] || [ "$cwd" = "." ]; then
+  cwd=$(nvr --remote-expr 'getcwd()' 2>/dev/null)
+fi
+
+if [ -n "$cwd" ] && [ -d "$cwd" ]; then
+  yazi "$cwd"
+else
+  yazi
+fi
