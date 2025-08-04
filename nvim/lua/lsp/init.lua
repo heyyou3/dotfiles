b3d59362578143dab9090ad5ecca602f88a626ae
heyyou <hey3you3@gmail.com>
2025-08-05 00:32:10 +0900

    (nvim)add vtsls

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,3 +1,7 @@
 local lua_ls_opts = require('lsp.lua_ls')
 vim.lsp.config('lua_ls', lua_ls_opts)
 vim.lsp.enable('lua_ls')
+
+local vtsls_opts = require('lsp.vtsls')
+vim.lsp.config('vtsls', vtsls_opts)
+vim.lsp.enable('vtsls')
