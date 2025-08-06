8f022341eb518f4e88d5205ef8949faba0191d7b
heyyou <hey3you3@gmail.com>
2025-08-06 21:16:24 +0900

    (nvim)change ui

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,39 +1,49 @@
-
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          -- レイアウトとサイズに関する設定
-          layout_strategy = "horizontal",
-          layout_config = {
-            width = 0.95,
-            height = 0.90,
-          },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"MunifTanjim/nui.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					-- レイアウトとサイズに関する設定
+					layout_strategy = "horizontal",
+					layout_config = {
+						prompt_position = "bottom",
+						preview_cutoff = 120,
+					},
+					borderchars = { "-", "|", "-", "|", "+", "+", "+", "|" },
 
-          borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
-
-          sorting_strategy = "ascending",
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--column",
-            "--hidden",
-            "--line-number",
-            "--no-heading",
-            "--smart-case",
-            "--with-filename",
-          },
-        },
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
-      })
-    end,
-  },
+					sorting_strategy = "ascending",
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--column",
+						"--hidden",
+						"--line-number",
+						"--no-heading",
+						"--smart-case",
+						"--with-filename",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_ivy({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,1 +1,23 @@
-return {}
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup({
+        -- your personnal icons can be defined here
+        -- you can specify a filetype name or the full file name
+        -- default = true will use the default icon provided
+        -- and override any icon defined in this setup function
+        override = {
+          -- zsh = {
+          --   icon = "",
+          --   color = "#428850",
+          --   name = "Zsh",
+          -- },
+        },
+        -- globally enable default icons (default to false)
+        -- will get overriden by `get_icons` option
+        default = true,
+      })
+    end,
+  },
+}
