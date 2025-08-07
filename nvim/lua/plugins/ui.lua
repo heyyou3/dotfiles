a1d97307c9de6f8ec6a05d8c0ef00945135cbf92
heyyou <hey3you3@gmail.com>
2025-08-08 08:24:54 +0900

    (nvim)add modes.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -20,4 +20,45 @@ return {
       })
     end,
   },
+  {
+      "mvllow/modes.nvim",
+      config = function()
+          require('modes').setup({
+              colors = {
+                bg = "#282a36",
+                copy = "#f1fa8c",
+                delete = "#ff5555",
+                change = "#ffb86c",
+                format = "#8be9fd",
+                insert = "#50fa7b",
+                replace = "#ff79c6",
+                select = "#bd93f9",
+                visual = "#bd93f9",
+            },
+
+            -- Set opacity for cursorline and number background
+            line_opacity = 0.15,
+
+            -- Enable cursor highlights
+            set_cursor = true,
+
+            -- Enable cursorline initially, and disable cursorline for inactive windows
+            -- or ignored filetypes
+            set_cursorline = true,
+
+
+            -- Enable line number highlights to match cursorline
+            set_number = true,
+
+
+            -- Enable sign column highlights to match cursorline
+            set_signcolumn = true,
+
+            -- Disable modes highlights for specified filetypes
+            -- or enable with prefix "!" if otherwise disabled (please PR common patterns)
+            -- Can also be a function fun():boolean that disables modes highlights when true
+            -- ignore = { 'NvimTree', 'TelescopePrompt', '!minifiles' }
+          })
+      end,
+  }
 }
