7edc0c4daae7d536f8b6c3b764e8bb97337eaade
heyyou <hey3you3@gmail.com>
2025-08-20 01:22:29 +0900

    (zx)web-search.mjs http, https の場合はリンク先へ遷移するように実装

--- zx/web-search.mjs
+++ zx/web-search.mjs
@@ -1,8 +1,12 @@
 #!/usr/bin/env zx
 
 try {
-  const query = await question('検索文字列: ');
+  const query = await question('search: ');
   const browser = process.env['BROWSER_BIN'];
+  if (/(^https:\/\/|^http:\/\/)/.test(query)) {
+    await $`${browser} ${query}`;
+    process.exit(0);
+  }
   const url = new URL(`https://google.com/search?q=${query}`).toString();
   await $`${browser} ${url}`;
   procese.exit(0);
