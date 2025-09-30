d2c57e68c7b0d20488cfb3ac084b5532f6713334
heyyou <hey3you3@gmail.com>
2025-10-01 03:16:04 +0900

    Nord テーマに変更
    - alacritty
    - git
    - nvim
    - tmux

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,85 +1,61 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = function()
-			local opts = {
-				options = {
-					theme = vim.g.colors_name,
-					refresh = {
-						statusline = 1000,
-					},
-					section_separators = "",
-					component_separators = "",
-				},
-				sections = {
-					lualine_a = { "branch" },
-					lualine_b = {
-						"location",
-					},
-					lualine_c = {
-						"diff",
-						"filetype",
-						{
-							"diagnostics",
-							source = { "nvim-lsp" },
-						},
-					},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = {},
-				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = {},
-				},
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = function()
+            local opts = {
+                options = {
+                    theme = vim.g.colors_name,
+                    refresh = {
+                        statusline = 1000,
+                    },
+                    section_separators = "",
+                    component_separators = "",
+                },
+                sections = {
+                    lualine_a = { "branch" },
+                    lualine_b = {
+                        "location",
+                    },
+                    lualine_c = {
+                        "diff",
+                        "filetype",
+                        {
+                            "diagnostics",
+                            source = { "nvim-lsp" },
+                        },
+                    },
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {},
+                },
 
-				tabline = {},
-				extensions = {},
-			}
-			return opts
-		end,
-	},
-	{ "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
-	{
-		"maxmx03/dracula.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			---@type dracula
-			local dracula = require("dracula")
-
-			dracula.setup({
-				transparent = false,
-				plugins = {
-					["nvim-treesitter"] = true,
-					["rainbow-delimiters"] = true,
-					["nvim-lspconfig"] = true,
-					["nvim-navic"] = true,
-					["nvim-cmp"] = true,
-					["indent-blankline.nvim"] = true,
-
-					["neo-tree.nvim"] = true,
-					["nvim-tree.lua"] = true,
-					["which-key.nvim"] = true,
-					["dashboard-nvim"] = true,
-					["gitsigns.nvim"] = true,
-
-					["neogit"] = true,
-					["todo-comments.nvim"] = true,
-					["lazy.nvim"] = true,
-					["telescope.nvim"] = true,
-					["noice.nvim"] = true,
-					["hop.nvim"] = true,
-					["mini.starter"] = true,
-					["mini.cursorword"] = true,
-					["bufferline.nvim"] = true,
-				},
-			})
-			vim.cmd.colorscheme("dracula")
-		end,
-	},
+                tabline = {},
+                extensions = {},
+            }
+            return opts
+        end,
+    },
+    { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+    {
+        "shaunsingh/nord.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = false
+            vim.g.nord_disable_background = false
+            vim.g.nord_italic = false
+            vim.g.nord_uniform_diff_background = true
+            vim.g.nord_bold = false
+            vim.cmd.colorscheme("nord")
+        end,
+    },
 }
