92cd41f41602c06c7d89d8451bddc635e2a83ec6
heyyou <hey3you3@gmail.com>
2025-10-15 09:16:46 +0900

    (nvim)入力補完の確定がエンターキーでできなくなっていたため、修正

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -1,1 +1,9 @@
-return { }
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+}
