37b90b32eb6eeb44e295efbb792dd3dbcb020922
heyyou <hey3you3@gmail.com>
2025-10-19 04:21:57 +0900

    (nvim)style lua

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,4 +1,3 @@
-
 -- Highlight full-width spaces and other special characters
 vim.api.nvim_set_hl(0, "ZenkakuSpace", { reverse = true, ctermfg = "DarkMagenta" })
 vim.api.nvim_set_hl(0, "SpecialKey", { ctermfg = "darkmagenta" })
@@ -6,47 +5,35 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" }) -- Make back
 
 local zenkaku_augroup = vim.api.nvim_create_augroup("ZenkakuSpaceGroup", { clear = true })
 vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "ColorScheme" }, {
-  group = zenkaku_augroup,
-  pattern = "*",
-  callback = function()
-    -- Re-apply highlight definition in case colorscheme cleared it
-    vim.api.nvim_set_hl(0, "ZenkakuSpace", { reverse = true, ctermfg = "DarkMagenta" })
-    -- The pattern is the full-width space character
-    vim.fn.matchadd("ZenkakuSpace", "　")
-  end,
+    group = zenkaku_augroup,
+    pattern = "*",
+    callback = function()
+        -- Re-apply highlight definition in case colorscheme cleared it
+        vim.api.nvim_set_hl(0, "ZenkakuSpace", { reverse = true, ctermfg = "DarkMagenta" })
+        -- The pattern is the full-width space character
+        vim.fn.matchadd("ZenkakuSpace", "　")
+    end,
 })
 
 -- General Autocommands
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.md",
-  command = "set filetype=markdown",
-})
-
--- Set conceallevel for markdown files to enable obsidian.nvim UI features.
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.conceallevel = 2
-  end,
+    pattern = "*.md",
+    command = "set filetype=markdown",
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = "*",
-  command = "set nopaste",
+    pattern = "*",
+    command = "set nopaste",
 })
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-  pattern = "*grep*",
-  command = "cwindow",
-})
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinEnter" }, {
-  pattern = "*",
-  command = "checktime",
+    pattern = "*grep*",
+    command = "cwindow",
 })
 
 -- Git commit diff view
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "gitcommit",
-  command = "DiffGitCached | wincmd x | resize 10",
+    pattern = "gitcommit",
+    command = "DiffGitCached | wincmd x | resize 10",
 })
 
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
