5ebf3b2544c67154a50900ab9c7f469cda808eef
heyyou <hey3you3@gmail.com>
2025-10-20 07:57:12 +0900

    (nvim)lsp の読み込み方法を変更

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,17 +1,26 @@
-
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = "BufReadPre",
-    config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufReadPre",
+        config = function()
+            -- Use vim.lsp.config as required by the deprecation warning
 
-      -- To add a new language server, install it and then add a setup call.
-      -- Example for rust-analyzer:
-      -- lspconfig.rust_analyzer.setup{ capabilities = capabilities }
-      -- Example for gopls:
-      -- lspconfig.gopls.setup{ capabilities = capabilities }
-    end,
-  },
+            -- Load custom settings from files in lsp/
+            local lua_ls_opts = require("lsp.lua_ls")
+            local vtsls_opts = require("lsp.vtsls")
+            local gopls_opts = require("lsp.gopls")
+            local terraform_opts = require("lsp.terraform_ls")
+
+            -- Configure servers
+            vim.lsp.config("lua_ls", lua_ls_opts)
+            vim.lsp.config("vtsls", vtsls_opts)
+            vim.lsp.config("gopls", gopls_opts)
+            vim.lsp.config("terraformls", terraform_opts) -- Use 'terraformls' as the server name
+
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("vtsls")
+            vim.lsp.enable("gopls")
+            vim.lsp.enable("terraformls")
+        end,
+    },
 }
