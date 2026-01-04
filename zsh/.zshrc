d7e199827ab905901087db830151942e166c304b
heyyou <hey3you3@gmail.com>
2026-01-05 00:00:11 +0900

    (zsh)zim から zinit に変更し、zeno.zsh を導入しました

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -11,27 +11,6 @@ setopt share_history
 setopt hist_ignore_dups
 setopt EXTENDED_HISTORY
 
-function select_history() {
-  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="history > ")
-  CURSOR=$#BUFFER
-}
-zle -N select_history
-bindkey '^r' select_history
-
-# ZIM SETTING START
-zstyle ':zim:zmodule' use 'degit'
-ZIM_HOME=~/.zim
-
-if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
-  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
-      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-fi
-
-if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
-  source ${ZIM_HOME}/zimfw.zsh init -q
-fi
-
-source ${ZIM_HOME}/init.zsh
-# ZIM SETTING END
-
 source "$DOT_FILES_PATH/common_sh/common"
+source "$DOT_FILES_PATH/zsh/zinit/main.zsh"
+
---
+++ zsh/zinit/main.zsh
@@ -0,0 +1,83 @@
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+zinit ice lucid depth"1" blockf
+zinit light yuki-yano/zeno.zsh
+
+# if defined load the configuration file from there
+# export ZENO_HOME=~/.config/zeno
+
+# if disable deno cache command when plugin loaded
+# export ZENO_DISABLE_EXECUTE_CACHE_COMMAND=1
+
+# if enable fzf-tmux
+# export ZENO_ENABLE_FZF_TMUX=1
+
+# if setting fzf-tmux options
+# export ZENO_FZF_TMUX_OPTIONS="-p"
+
+# by default a unix domain socket is used
+# if disable it
+# export ZENO_DISABLE_SOCK=1
+
+# if disable builtin completion
+# export ZENO_DISABLE_BUILTIN_COMPLETION=1
+
+# default
+export ZENO_GIT_CAT="cat"
+# git file preview with color
+# export ZENO_GIT_CAT="bat --color=always"
+
+# default
+export ZENO_GIT_TREE="tree"
+# git folder preview with color
+# export ZENO_GIT_TREE="eza --tree"
+
+if [[ -n $ZENO_LOADED ]]; then
+  bindkey ' '  zeno-auto-snippet
+
+  # fallback if snippet not matched (default: self-insert)
+  # export ZENO_AUTO_SNIPPET_FALLBACK=self-insert
+
+  # if you use zsh's incremental search
+  # bindkey -M isearch ' ' self-insert
+
+  bindkey '^m' zeno-auto-snippet-and-accept-line
+
+  bindkey '^i' zeno-completion
+
+  bindkey '^j' zeno-insert-snippet           # open snippet picker (fzf) and insert at cursor
+
+  bindkey '^x '  zeno-insert-space
+  bindkey '^x^m' accept-line
+  bindkey '^x^z' zeno-toggle-auto-snippet
+
+  # preprompt bindings
+  bindkey '^xp' zeno-preprompt
+  bindkey '^xs' zeno-preprompt-snippet
+  # Outside ZLE you can run `zeno-preprompt git {{cmd}}` or `zeno-preprompt-snippet foo`
+  # to set the next prompt prefix; invoking them with an empty argument resets the state.
+
+  bindkey '^r' zeno-history-selection         # classic history widget
+  # bindkey '^r' zeno-smart-history-selection # smart history widget
+
+  # fallback if completion not matched
+  # (default: fzf-completion if exists; otherwise expand-or-complete)
+  # export ZENO_COMPLETION_FALLBACK=expand-or-complete
+fi
+
